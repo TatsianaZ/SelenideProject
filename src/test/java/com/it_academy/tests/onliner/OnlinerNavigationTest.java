718410276023191a1f4b9e4de40928900448026b
tzhdanko1 <tzhdanko1@gmail.com>
2022-03-23 16:15:45 +0100

    test refactoring

--- src/test/java/com/it_academy/tests/onliner/OnlinerNavigationTest.java
+++ src/test/java/com/it_academy/tests/onliner/OnlinerNavigationTest.java
@@ -3,11 +3,13 @@ package com.it_academy.tests.onliner;
 import com.it_academy.tests.BaseTest;
 import com.it_academy.tests.onliner.pageobject.onliner.HomePage;
 import com.it_academy.tests.onliner.pageobject.onliner.OnlinerHeader;
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 
 public class OnlinerNavigationTest extends BaseTest {
 
     @Test
+    @Description("test the user is able to navigate to Product Page via Catalog")
     public void testUserIsAbleToNavigateToProductPageViaCatalog() {
         new HomePage().navigateToOnlinerHomePage();
         new OnlinerHeader()

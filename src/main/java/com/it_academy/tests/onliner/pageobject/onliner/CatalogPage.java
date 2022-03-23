a8570a3c708dc5c53d93c24171f29f521b5b96ee
tzhdanko1 <tzhdanko1@gmail.com>
2022-03-23 16:21:36 +0100

    refactoring

--- src/main/java/com/it_academy/tests/onliner/pageobject/onliner/CatalogPage.java
+++ src/main/java/com/it_academy/tests/onliner/pageobject/onliner/CatalogPage.java
@@ -17,21 +17,21 @@ public class CatalogPage extends BasePage {
     private static final String PRODUCT_LINK_XPATH_PATTERN =
             "//*[contains(@class, 'item_active')]//a[.//*[contains(text(), '%s')]]";
 
-    @Step("Click on catalog classifier link")
+    @Step("Click on catalog classifier link {link}")
     public CatalogPage clickOnCatalogClassifierLink(String link) {
         clickOnElementViaJs($x(format(CATALOG_CLASSIFIER_LINK_XPATH_PATTERN, link))
                 .shouldBe(visible, Duration.ofSeconds(10)));
         return this;
     }
 
-    @Step("Select category")
+    @Step("Select category {category}")
     public CatalogPage selectCategory(String category) {
         clickOnElementViaJs($x(format(CATALOG_CLASSIFIER_CATEGORY_XPATH_PATTERN, category))
                 .shouldBe(visible, Duration.ofSeconds(10)));
         return this;
     }
 
-    @Step("Select product")
+    @Step("Select product {product}")
     public ProductPage selectProduct(String product) {
         clickOnElementViaJs($x(format(PRODUCT_LINK_XPATH_PATTERN, product))
                 .shouldBe(visible, Duration.ofSeconds(10)));

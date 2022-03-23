fd887a07a7799a9d1d48d69348ce3de4eaa2b442
tzhdanko1 <tzhdanko1@gmail.com>
2022-03-23 10:59:27 +0100

    added a new onliner test

--- src/test/java/com/it_academy/onliner/NavigationTest.java
+++ src/test/java/com/it_academy/onliner/NavigationTest.java
@@ -3,6 +3,8 @@ package com.it_academy.onliner;
 import com.it_academy.onliner.pageobject.HomePage;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class NavigationTest {
 
     @Test
@@ -11,5 +13,10 @@ public class NavigationTest {
                 .clickOnMenuItem("Docs")
                 .clickOnGherkinSyntaxSubMenuItem()
                 .assertPageWithTitleDisplayed("Gherkin Syntax");
+    }
+
+    @Test
+    public void testOnlinerNavigation() {
+        open("https://www.onliner.by/");
     }
 }
